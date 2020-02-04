054554cc5a4048b5c09056cbd45adef51b6362a9
jDanielDetes <jdanieldetes@gmail.com>
2020-02-04 18:55:50 -0500

    unfavorite button working

--- app/src/Reducers/favoriteReducer.js
+++ app/src/Reducers/favoriteReducer.js
@@ -5,10 +5,10 @@ const initialState={
         favoriteList:[]
     },
     dummySongs:[
-        {name:"Song1"},
-        {name:"Song2"},
-        {name:'Song3'},
-        {name:"Song4"}
+        {id:1,name:"Song1"},
+        {id:2,name:"Song2"},
+        {id:3,name:'Song3'},
+        {id:4,name:"Song4"}
     ]
 
    
@@ -23,15 +23,16 @@ export  const favoriteReducer =(state=initialState,action)=>{
         case 'ADD_FAVORITE':
             console.log('from the reducer', action.payload);
             return{...state,
-                Favorites: { ...state.Favorites, favoriteList: state.Favorites.favoriteList.includes(action.payload) ? [...state.Favorites.favoriteList] :[...state.Favorites.favoriteList,action.payload]}}
+                Favorites: { ...state.Favorites, favoriteList: state.Favorites.favoriteList.includes(action.payload) ? [...state.Favorites.favoriteList] : [...state.Favorites.favoriteList,action.payload]}}
 
 
             case 'REMOVE_FAVORITE':
+                console.log('from the reducer444', action.payload);
                 return {
                     ...state,
                     Favorites: {
                       ...state.Favorites,
-                      features: state.Favorites.favoriteList.filter(song => song.id !==action.payload.id)
+                      favoriteList: state.Favorites.favoriteList.filter(song => song.id !== action.payload.id)
                     }
                   };  
             default:
