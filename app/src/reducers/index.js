4784634897043f79a37a8da71f2e2d0ca023e6d0
jDanielDetes <jdanieldetes@gmail.com>
2020-02-06 10:31:25 -0500

    suggested list 90% complete network error

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux'
 import {searchReducer} from './searchReducer'
 import {favoriteReducer} from './favoriteReducer'
+import {suggestedReducer} from './suggestedReducer'
 
 export default combineReducers({
     searchReducer,
-    favoriteReducer
+    favoriteReducer,
+    suggestedReducer
     //add other reducers here, comma separated
 });
--- app/src/reducers/suggestedReducer.js
+++ app/src/reducers/suggestedReducer.js
@@ -1,13 +1,28 @@
-const initialState-{
-    Suggested:[]
+
+const initialState={
+    Suggested:[],
+    error:''
 }
 
 
 export const suggestedReducer=(state=initialState,action) =>{
     switch(action.type){
-        case SUGG_GET:
+        case "SUGG_START":
             return {
-                
+                ...state
             }
+            case "SUGG_SUCCESS":
+                return{
+                    ...state,
+                    Suggested:[...action.payload]
+                }
+                case "SUGG_FAILURE":
+                    return{
+                        ...state,
+                        error: action.payload
+                    }
+
+                    default:
+                        return state
     }
 }
