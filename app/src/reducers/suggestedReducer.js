4f321838bd9601941fc0255cdee6ec76d199d7f0
jDanielDetes <jdanieldetes@gmail.com>
2020-02-05 22:53:40 -0500

    working on suggested songs

---
+++ app/src/reducers/suggestedReducer.js
@@ -0,0 +1,13 @@
+const initialState-{
+    Suggested:[]
+}
+
+
+export const suggestedReducer=(state=initialState,action) =>{
+    switch(action.type){
+        case SUGG_GET:
+            return {
+                
+            }
+    }
+}
